ef0a7c81af5fa306f3112fcb6f83a6320c32b0b5
Wyllman <wyllman@gmail.com>
2015-02-10 01:18:19 +0000

    Mejorando el script global.js. Resaltando cabeceras de asignaturas.

--- scripts/global.js
+++ scripts/global.js
@@ -1,6 +1,13 @@
 
 window.onload = function () {
    recolocarCuerpo ();
+   var listaResaltar = document.getElementsByTagName("LABEL");
+   var listaTam = listaResaltar.length;
+   for (var i = 0; i < listaTam; ++i) {
+      listaResaltar[i].addEventListener("mouseover", resaltarTexto);
+      listaResaltar[i].addEventListener("mouseout", normalizarTexto);
+      listaResaltar[i].addEventListener("click", clickTexto);
+   }
 }
 
 window.onresize = function () {
@@ -12,4 +19,25 @@ function recolocarCuerpo () {
    var cuerpo = document.getElementById("cuerpo");
    
    cuerpo.style.marginTop = cabecera.offsetHeight + "px";
+}
+
+function resaltarTexto () {
+   this.className = "resaltado";
+}
+
+function normalizarTexto () {
+   this.className = "";
+}
+
+function clickTexto () {
+   var texto = this.nextElementSibling.className;
+   
+   if (texto.match(/oculto/)){
+      texto = texto.replace(/(\,\s)?oculto/, " ");
+   } else {
+      texto += " oculto";
+   }
+   
+   this.nextElementSibling.className = texto;
+
 }
