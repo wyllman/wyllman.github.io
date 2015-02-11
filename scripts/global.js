caf1e86be0a79e742096bf1ae177c1fdb32462f2
Wyllman <wyllman@gmail.com>
2015-02-11 01:08:45 +0000

    creada la cabeceraMini, creadas las transiciones para ocultar la cabecera y añadidas compatibilidades con otros navegadores.

--- scripts/global.js
+++ scripts/global.js
@@ -1,13 +1,39 @@
+var OLD_NAV = false;
+
 
 window.onload = function () {
    recolocarCuerpo ();
    var listaResaltar = document.getElementsByTagName("LABEL");
    var listaTam = listaResaltar.length;
+   var cabecera = document.getElementById("cabecera");
+   
+   
    for (var i = 0; i < listaTam; ++i) {
-      listaResaltar[i].addEventListener("mouseover", resaltarTexto);
-      listaResaltar[i].addEventListener("mouseout", normalizarTexto);
       listaResaltar[i].addEventListener("click", clickTexto);
    }
+   
+   cabecera.addEventListener("click", subirCabecera);
+   
+   
+   /* Listen for a transition! */
+   var transitionEvent = whichTransitionEvent();
+   if (transitionEvent) {
+
+      cabecera.addEventListener(transitionEvent, function() {
+          recolocarCuerpo ();
+      });
+   }
+   /*crear una hoja de estilo propia -----
+   
+   var style = document.createElement('style');
+   style.type = 'text/css';
+   style.innerHTML = '.cssClass { color: #F00; }';
+   document.getElementsByTagName('head')[0].appendChild(style);
+
+   document.getElementById('someElementId').className = 'cssClass';
+   
+   */
+
 }
 
 window.onresize = function () {
@@ -18,15 +44,36 @@ function recolocarCuerpo () {
    var cabecera = document.getElementById("cabecera");
    var cuerpo = document.getElementById("cuerpo");
    
-   cuerpo.style.marginTop = cabecera.offsetHeight + "px";
-}
-
-function resaltarTexto () {
-   this.className = "resaltado";
-}
-
-function normalizarTexto () {
-   this.className = "";
+   var valor = cabecera.offsetHeight + cabecera.offsetTop + 5;
+   
+   
+   	var elemento = document.getElementById("estiloCuerpo");
+    if (!elemento){
+       console.log("El elemento selecionado no existe");
+    } else {
+       padre = elemento.parentNode;
+       padre.removeChild(elemento);
+    }
+   
+   var style = document.createElement('style');
+   style.id = "estiloCuerpo";
+   style.type = 'text/css';
+   style.innerHTML = ".ctrans { transition: margin-top 1s;"
+                            + "-webkit-transition: margin-top 1s;"
+                            + " margin-top: " +  valor + "px }";
+                            
+   cuerpo.appendChild(style);
+   
+   var texto = cuerpo.className;
+   
+   if (texto.match(/ctrans/)){
+      //texto = texto.replace(/(\,\s)?ctrans/, " ");
+   } else {
+      texto += " ctrans";
+   }
+   
+   cuerpo.className = texto;
+   
 }
 
 function clickTexto () {
@@ -40,4 +87,78 @@ function clickTexto () {
    
    this.nextElementSibling.className = texto;
 
+}
+
+function subirCabecera () {
+  //console.log("funiciona?");
+  
+  var cabecera = document.getElementById("cabecera");
+  var cabeceraMini = document.getElementById("cabeceraMini");
+  var texto = cabecera.className;
+  var texto2 = cabeceraMini.className;
+  var valor = - (cabecera.offsetHeight - 20) ;//+ cabecera.offsetTop)
+  
+   	var elemento = document.getElementById("estiloCabecera");
+    if (!elemento){
+       console.log("El elemento selecionado no existe");
+    } else {
+       padre = elemento.parentNode;
+       padre.removeChild(elemento);
+    }
+   
+   var style = document.createElement('style');
+   style.id = "estiloCabecera";
+   style.type = 'text/css';
+   style.innerHTML = "#cabecera.htrans { transition: top 2s;"
+                            + "-webkit-transition: top 2s;"
+                            + " top: " +  valor + "px }";
+   cabecera.appendChild(style);
+   
+   
+   if (texto.match(/htransR/)){
+      texto = texto.replace(/(\,\s)?htransR/, "");
+   }
+   
+   
+   if (texto.match(/htrans/)){
+      texto = texto.replace(/(\,\s)?htrans/, "");
+      texto += " htransR";
+   } else {
+      texto += " htrans";
+   }
+   
+   if (texto2.match(/cmtransR/)){
+      texto2 = texto2.replace(/(\,\s)?cmtransR/, "");
+   }
+   
+   
+   if (texto2.match(/cmtrans/)){
+      texto2 = texto2.replace(/(\,\s)?cmtrans/, "");
+      texto2 += " cmtransR";
+   } else {
+      texto2 += " cmtrans";
+   }
+   
+   cabecera.className = texto;
+   cabeceraMini.className = texto2;
+}
+
+
+
+/* From Modernizr */
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'WebkitTransition':'webkitTransitionEnd',
+      'MozTransition':'transitionend'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
 }
